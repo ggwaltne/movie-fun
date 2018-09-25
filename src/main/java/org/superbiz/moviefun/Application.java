5cdf20806a755f1bf64c99b8689d33bb5bc00c4d
Gage <ggwaltne@harding.edu>
2018-09-25 09:11:25 -0600

    Stop forgetting to commit

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,24 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ActionServlet actionServlet)
+    {
+        ServletRegistrationBean srb = new ServletRegistrationBean(actionServlet);
+        srb.addUrlMappings("/moviefun/*");
+        return srb;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
